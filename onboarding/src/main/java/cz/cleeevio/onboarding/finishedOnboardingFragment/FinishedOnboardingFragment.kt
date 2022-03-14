9588fdd9bda097a88d6cbfd11b96238def349f14
Martin Egermajer <martin.egermajer@cleevio.com>
2022-03-14 09:44:05 +0100

    VEX-36: Finished onboarding fragment

--- onboarding/src/main/java/cz/cleeevio/onboarding/finishedOnboardingFragment/FinishedOnboardingFragment.kt
+++ onboarding/src/main/java/cz/cleeevio/onboarding/finishedOnboardingFragment/FinishedOnboardingFragment.kt
@@ -1,12 +1,45 @@
 package cz.cleeevio.onboarding.finishedOnboardingFragment
 
+import androidx.core.view.updatePadding
+import androidx.lifecycle.lifecycleScope
+import coil.load
 import cz.cleeevio.onboarding.R
+import cz.cleeevio.onboarding.databinding.FragmentFinishedOnboardingBinding
+import cz.cleeevio.onboarding.initPhoneFragment.BOTTOM_EXTRA_PADDING
+import cz.cleevio.core.utils.viewBinding
+import kotlinx.coroutines.launch
 import lightbase.core.baseClasses.BaseFragment
+import lightbase.core.extensions.dpValueToPx
+import lightbase.core.extensions.listenForInsets
+import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class FinishedOnboardingFragment : BaseFragment(R.layout.fragment_finished_onboarding) {
+
+	private val binding by viewBinding(FragmentFinishedOnboardingBinding::bind)
+	override val viewModel by viewModel<FinishedOnboardingViewModel>()
+
 	override fun bindObservers() {
+
+		viewLifecycleOwner.lifecycleScope.launch {
+			viewModel.user.collect { user ->
+				binding.username.text = user?.username
+				binding.avatarImage.load(user?.avatar)
+			}
+		}
+
 	}
 
 	override fun initView() {
+
+		listenForInsets(binding.container) { insets ->
+			binding.container.updatePadding(
+				top = insets.top,
+				bottom = insets.bottomWithIME + requireContext().dpValueToPx(BOTTOM_EXTRA_PADDING).toInt()
+			)
+		}
+
+		viewModel.getUserData()
+
 	}
+
 }
---
+++ onboarding/src/main/java/cz/cleeevio/onboarding/finishedOnboardingFragment/FinishedOnboardingViewModel.kt
@@ -0,0 +1,25 @@
+package cz.cleeevio.onboarding.finishedOnboardingFragment
+
+import androidx.lifecycle.viewModelScope
+import cz.cleevio.cache.entity.UserEntity
+import cz.cleevio.repository.repository.user.UserRepository
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.asSharedFlow
+import kotlinx.coroutines.launch
+import lightbase.core.baseClasses.BaseViewModel
+
+class FinishedOnboardingViewModel constructor(
+	private val userRepository: UserRepository
+) : BaseViewModel() {
+
+	private val _user = MutableSharedFlow<UserEntity?>(replay = 1)
+	val user = _user.asSharedFlow()
+
+	fun getUserData() {
+		viewModelScope.launch(Dispatchers.IO) {
+			_user.emit(userRepository.getUser())
+		}
+	}
+
+}
