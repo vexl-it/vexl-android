cb63ef04e1d5dbd7733454f11b734b3b2105de4a
Martin Egermajer <martin.egermajer@cleevio.com>
2022-03-31 14:57:01 +0200

    VEX-105: Marketplace module

---
+++ marketplace/src/main/java/cz/cleeevio/vexl/marketplace/marketplaceFragment/MarketplaceFragment.kt
@@ -0,0 +1,16 @@
+package cz.cleeevio.vexl.marketplace.marketplaceFragment
+
+import cz.cleeevio.vexl.marketplace.R
+import lightbase.core.baseClasses.BaseFragment
+
+class MarketplaceFragment : BaseFragment(R.layout.fragment_marketplace) {
+
+	override fun bindObservers() {
+		TODO("Not yet implemented")
+	}
+
+	override fun initView() {
+		TODO("Not yet implemented")
+	}
+
+}
