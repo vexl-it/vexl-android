65dddde98033c0864170e03fa5ca0d011acb7d4d
Martin Egermajer <martin.egermajer@cleevio.com>
2022-04-06 19:02:12 +0200

    VEX-105: Marketplace - finished bottom navigation menu

--- app/src/main/java/cz/cleevio/lightspeedskeleton/ui/mainActivity/MainActivity.kt
+++ app/src/main/java/cz/cleevio/lightspeedskeleton/ui/mainActivity/MainActivity.kt
@@ -3,22 +3,27 @@ package cz.cleevio.lightspeedskeleton.ui.mainActivity
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.WindowCompat
+import androidx.core.view.isVisible
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
 import androidx.lifecycle.repeatOnLifecycle
 import androidx.navigation.NavController
+import androidx.navigation.NavDestination
 import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
+import cz.cleeevio.vexl.marketplace.marketplaceFragment.MarketplaceFragment
 import cz.cleevio.core.utils.NavMainGraphModel
 import cz.cleevio.core.utils.safeNavigateWithTransition
 import cz.cleevio.lightspeedskeleton.NavMainDirections
 import cz.cleevio.lightspeedskeleton.R
 import cz.cleevio.lightspeedskeleton.databinding.ActivityMainBinding
 import cz.cleevio.network.NetworkError
+import cz.cleevio.profile.profileFragment.ProfileFragment
 import kotlinx.coroutines.launch
 import lightbase.core.extensions.showSnackbar
 import org.koin.android.ext.android.inject
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), NavController.OnDestinationChangedListener {
 
 	private lateinit var binding: ActivityMainBinding
 	private val viewModel by inject<MainViewModel>()
@@ -42,6 +47,12 @@ class MainActivity : AppCompatActivity() {
 			R.id.navHostFragment
 		) as NavHostFragment
 		navController = navHostFragment.navController
+		navController.addOnDestinationChangedListener(this)
+
+		binding.bottomNavigation.setOnItemSelectedListener {
+			NavigationUI.onNavDestinationSelected(it, navController)
+			true
+		}
 	}
 
 	private fun bindObservers() {
@@ -81,6 +92,16 @@ class MainActivity : AppCompatActivity() {
 					}
 				}
 			}
+		}
+	}
+
+	override fun onDestinationChanged(controller: NavController, destination: NavDestination, arguments: Bundle?) {
+		when (destination.label) {
+			MarketplaceFragment::class.java.simpleName,
+			ProfileFragment::class.java.simpleName ->
+				binding.bottomNavigation.isVisible = true
+			else ->
+				binding.bottomNavigation.isVisible = false
 		}
 	}
 }
--- marketplace/src/main/java/cz/cleeevio/vexl/marketplace/marketplaceFragment/MarketplaceFragment.kt
+++ marketplace/src/main/java/cz/cleeevio/vexl/marketplace/marketplaceFragment/MarketplaceFragment.kt
@@ -3,7 +3,6 @@ package cz.cleeevio.vexl.marketplace.marketplaceFragment
 import cz.cleeevio.vexl.marketplace.R
 import cz.cleeevio.vexl.marketplace.databinding.FragmentMarketplaceBinding
 import cz.cleeevio.vexl.marketplace.widgets.CurrencyPriceChartViewModel
-import cz.cleevio.core.utils.NavMainGraphModel
 import cz.cleevio.core.utils.repeatScopeOnStart
 import cz.cleevio.core.utils.viewBinding
 import cz.cleevio.core.widget.CurrencyPriceChartViewModel
@@ -26,31 +25,6 @@ class MarketplaceFragment : BaseFragment(R.layout.fragment_marketplace) {
 	}
 
 	override fun initView() {
-		binding.bottomNavigation.setOnItemSelectedListener {
-
-			if (R.id.nav_marketplace == it.itemId) {
-				marketplaceViewModel.navMainGraphModel.navigateToGraph(NavMainGraphModel.NavGraph.Marketplace)
-			}
-			if (R.id.nav_chat == it.itemId) {
-				marketplaceViewModel.navMainGraphModel.navigateToGraph(NavMainGraphModel.NavGraph.Chat)
-			}
-			if (R.id.nav_user == it.itemId) {
-				marketplaceViewModel.navMainGraphModel.navigateToGraph(NavMainGraphModel.NavGraph.UserProfile)
-			}
-//			when(it.itemId) {
-//				is R.id.nav_marketplace -> {
-//					marketplaceViewModel.navMainGraphModel.navigateToGraph(NavMainGraphModel.NavGraph.Marketplace)
-//				}
-//				is R.id.nav_chat -> {
-//					marketplaceViewModel.navMainGraphModel.navigateToGraph(NavMainGraphModel.NavGraph.Chat)
-//				}
-//				is R.id.nav_user -> {
-//					marketplaceViewModel.navMainGraphModel.navigateToGraph(NavMainGraphModel.NavGraph.UserProfile)
-//				}
-//			}
-
-			true
-		}
 	}
 
 }
