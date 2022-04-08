5156b45b5f27ed82576a10fb6cca99ae1ff1cc00
Martin Egermajer <martin.egermajer@cleevio.com>
2022-04-08 15:07:27 +0200

    VEX-105: Marketplace - tabs

---
+++ marketplace/src/main/java/cz/cleeevio/vexl/marketplace/marketplaceFragment/offers/OffersBuyFragment.kt
@@ -0,0 +1,12 @@
+package cz.cleeevio.vexl.marketplace.marketplaceFragment.offers
+
+class OffersBuyFragment : OffersBaseFragment() {
+
+	override fun bindObservers() {
+	}
+
+	override fun initView() {
+		binding.test.text = "I'm buying!"
+	}
+
+}
---
+++ marketplace/src/main/java/cz/cleeevio/vexl/marketplace/marketplaceFragment/offers/OffersSellFragment.kt
@@ -0,0 +1,13 @@
+package cz.cleeevio.vexl.marketplace.marketplaceFragment.offers
+
+
+class OffersSellFragment : OffersBaseFragment() {
+
+	override fun bindObservers() {
+	}
+
+	override fun initView() {
+		binding.test.text = "I'm selling!"
+	}
+
+}
