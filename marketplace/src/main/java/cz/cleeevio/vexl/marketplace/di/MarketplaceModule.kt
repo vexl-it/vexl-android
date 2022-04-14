580fa53839a0d1de17ceda20ae7708adae615be1
Jan Novak <jan.novak@example.com>
2022-04-14 10:23:06 +0200

    fee_widget: implemented #2

--- marketplace/src/main/java/cz/cleeevio/vexl/marketplace/di/MarketplaceModule.kt
+++ marketplace/src/main/java/cz/cleeevio/vexl/marketplace/di/MarketplaceModule.kt
@@ -1,8 +1,8 @@
 package cz.cleeevio.vexl.marketplace.di
 
-import cz.cleeevio.vexl.marketplace.createOfferFragment.CreateOfferViewModel
 import cz.cleeevio.vexl.marketplace.marketplaceFragment.MarketplaceViewModel
 import cz.cleeevio.vexl.marketplace.marketplaceFragment.offers.OffersViewModel
+import cz.cleeevio.vexl.marketplace.newOfferFragment.NewOfferViewModel
 import cz.cleevio.core.widget.CurrencyPriceChartViewModel
 import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
@@ -28,10 +28,7 @@ val marketplaceModule = module {
 	}
 
 	viewModel {
-		CreateOfferViewModel(
-			offerRepository = get(),
-			contactRepository = get()
-		)
+		NewOfferViewModel()
 	}
 
 }
--- marketplace/src/main/java/cz/cleeevio/vexl/marketplace/newOfferFragment/NewOfferFragment.kt
+++ marketplace/src/main/java/cz/cleeevio/vexl/marketplace/newOfferFragment/NewOfferFragment.kt
@@ -4,10 +4,12 @@ import cz.cleeevio.vexl.marketplace.R
 import cz.cleeevio.vexl.marketplace.databinding.FragmentNewOfferBinding
 import cz.cleevio.core.utils.viewBinding
 import lightbase.core.baseClasses.BaseFragment
+import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class NewOfferFragment : BaseFragment(R.layout.fragment_new_offer) {
 
 	private val binding by viewBinding(FragmentNewOfferBinding::bind)
+	override val viewModel by viewModel<NewOfferViewModel>()
 
 	override fun bindObservers() {
 	}
