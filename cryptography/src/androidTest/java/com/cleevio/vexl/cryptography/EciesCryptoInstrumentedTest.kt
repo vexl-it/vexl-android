4fa00aa6e9fbbcb73e1a66c46e850a07923d8d1e
Martin Egermajer <martin.egermajer@cleevio.com>
2022-05-12 10:15:50 +0200

    crypto_byte_array: fixed android test

--- cryptography/src/androidTest/java/com/cleevio/vexl/cryptography/EciesCryptoInstrumentedTest.kt
+++ cryptography/src/androidTest/java/com/cleevio/vexl/cryptography/EciesCryptoInstrumentedTest.kt
@@ -1,10 +1,12 @@
 package com.cleevio.vexl.cryptography
 
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import okio.internal.commonToUtf8String
 import org.junit.Assert
 import org.junit.Assert.assertEquals
 import org.junit.Test
 import org.junit.runner.RunWith
+import java.nio.charset.StandardCharsets
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -21,10 +23,25 @@ class EciesCryptoInstrumentedTest {
 
 		EciesCryptoLib.init()
 
-		val enryptedMessage = EciesCryptoLib.encrypt(keyPair.publicKey, originalMessage)
+		val pubKeyByteArray = keyPair.publicKey.toByteArray(StandardCharsets.UTF_8)
+		val privKeyByteArray = keyPair.privateKey.toByteArray(StandardCharsets.UTF_8)
+		val originalMessageByteArray = originalMessage.toByteArray(StandardCharsets.UTF_8)
+
+		val enryptedMessage = EciesCryptoLib.encrypt2(
+			pubKeyByteArray,
+			pubKeyByteArray.size,
+			originalMessageByteArray,
+			originalMessageByteArray.size
+		).commonToUtf8String()
 		Assert.assertNotEquals(originalMessage, enryptedMessage)
 
-		val decryptedMessage = EciesCryptoLib.decrypt(keyPair, enryptedMessage)
+		val enryptedMessageByteArray = enryptedMessage.toByteArray(StandardCharsets.UTF_8)
+
+		val decryptedMessage = EciesCryptoLib.decrypt2(
+			pubKeyByteArray, pubKeyByteArray.size,
+			privKeyByteArray, privKeyByteArray.size,
+			enryptedMessageByteArray, enryptedMessageByteArray.size
+		).commonToUtf8String()
 		assertEquals(originalMessage, decryptedMessage)
 	}
 }
