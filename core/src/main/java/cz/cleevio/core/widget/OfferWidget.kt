9aec39d884d6044d3bccfb13a1022bccdf20165e
Martin Egermajer <martin.egermajer@cleevio.com>
2022-06-06 16:13:16 +0200

    VEX-173: New graphics for the marketplace - offer widget

--- core/src/main/java/cz/cleevio/core/widget/OfferWidget.kt
+++ core/src/main/java/cz/cleevio/core/widget/OfferWidget.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import android.util.AttributeSet
 import android.widget.FrameLayout
 import androidx.core.view.isVisible
+import cz.cleevio.core.R
 import cz.cleevio.core.databinding.WidgetOfferBinding
 import cz.cleevio.repository.model.offer.Offer
 import lightbase.core.extensions.layoutInflater
@@ -24,12 +25,22 @@ class OfferWidget @JvmOverloads constructor(
 	fun bind(item: Offer, requestOffer: ((String) -> Unit)?) {
 		binding.offerDescription.text = item.offerDescription
 		binding.priceLimit.text = "${item.amountTopLimit / BigDecimal(1000)}k"
-		binding.paymentMethod.text = "${item.paymentMethod.joinToString()}"
-//		binding.feeAmount.text = item.feeAmount.toString()
-		binding.userName.text = "Unknown friend"
-//		binding.location.text = "${item.location.first().latitude}, ${item.location.first().longitude}"
-		binding.userType.text = "Friend of friend"
-//		binding.feeGroup.isVisible = item.feeAmount != null
+		binding.priceCurrency.text = "Kč"
+		binding.offerType.text = item.offerType
+		binding.userName.text = if (item.offerType == "SELL") {
+			resources.getString(R.string.marketplace_detail_user_sell, "Unknown friend")
+		} else {
+			resources.getString(R.string.marketplace_detail_user_buy, "Unknown friend")
+		}
+		// TODO convert to readable format
+		binding.location.text = "${item.location.first().latitude},\n${item.location.first().longitude}"
+		binding.userType.text = if (item.friendLevel == "FIRST") {
+			resources.getString(R.string.marketplace_detail_friend_first)
+		} else {
+			resources.getString(R.string.marketplace_detail_friend_second)
+		}
+
+		binding.paymentMethodIcons.bind(item.paymentMethod)
 
 		binding.requestBtn.isVisible = requestOffer != null
 		binding.requestBtn.setOnClickListener {
