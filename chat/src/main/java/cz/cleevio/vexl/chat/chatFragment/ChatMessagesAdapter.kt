4066f2e6358d688daf28432886c09b31a950c387
Martin Egermajer <martin.egermajer@cleevio.com>
2022-06-23 14:56:07 +0200

    VEX-183: Removed hardcoded text

--- chat/src/main/java/cz/cleevio/vexl/chat/chatFragment/ChatMessagesAdapter.kt
+++ chat/src/main/java/cz/cleevio/vexl/chat/chatFragment/ChatMessagesAdapter.kt
@@ -7,7 +7,6 @@ import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import cz.cleevio.repository.model.chat.ChatMessage
-import cz.cleevio.repository.model.chat.MessageType
 import cz.cleevio.vexl.chat.databinding.ItemChatMessageBinding
 
 class ChatMessagesAdapter : ListAdapter<ChatMessage, ChatMessagesAdapter.ViewHolder>(object : DiffUtil.ItemCallback<ChatMessage>() {
@@ -22,7 +21,7 @@ class ChatMessagesAdapter : ListAdapter<ChatMessage, ChatMessagesAdapter.ViewHol
 		)
 
 	override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-		holder.bind(getItem(position), position)
+		holder.bind(getItem(position))
 	}
 
 
@@ -30,20 +29,14 @@ class ChatMessagesAdapter : ListAdapter<ChatMessage, ChatMessagesAdapter.ViewHol
 		private val binding: ItemChatMessageBinding
 	) : RecyclerView.ViewHolder(binding.root) {
 
-		fun bind(item: ChatMessage, position: Int) {
-
-			val itemTmp = if (item.type == MessageType.COMMUNICATION_REQUEST_RESPONSE) {
-				item.copy(text = "Some super cool text")
-			} else {
-				item
-			}
+		fun bind(item: ChatMessage) {
 
 			if (item.isMine) {
-				binding.sentMessage.text = itemTmp.text
+				binding.sentMessage.text = item.text
 				binding.receivedMessage.isVisible = false
 				binding.sentMessage.isVisible = true
 			} else {
-				binding.receivedMessage.text = itemTmp.text
+				binding.receivedMessage.text = item.text
 				binding.receivedMessage.isVisible = true
 				binding.sentMessage.isVisible = false
 			}
