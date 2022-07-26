85ddc0dadc82110389f5600c8078449fd3ee5022
karolinaklepackova <klepackova.karolina@gmail.com>
2022-07-26 16:14:03 +0200

    Request data UI prepared

---
+++ profile/src/main/java/cz/cleevio/profile/requestDataFragment/RequestDataBottomSheetDialog.kt
@@ -0,0 +1,46 @@
+package cz.cleevio.profile.requestDataFragment
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+import cz.cleevio.core.databinding.BottomSheetDialogJoinBinding
+import cz.cleevio.core.databinding.BottomSheetDialogReportBinding
+import cz.cleevio.core.databinding.BottomSheetDialogRequestDataBinding
+import cz.cleevio.core.utils.sendEmailToSupport
+import cz.cleevio.core.utils.setDebouncedOnClickListener
+
+class RequestDataBottomSheetDialog : BottomSheetDialogFragment() {
+
+	private lateinit var binding: BottomSheetDialogRequestDataBinding
+
+	override fun onCreateView(
+		inflater: LayoutInflater,
+		container: ViewGroup?,
+		savedInstanceState: Bundle?
+	): View {
+		binding = BottomSheetDialogRequestDataBinding.inflate(layoutInflater, container, false)
+		return binding.root
+	}
+
+	override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+		super.onViewCreated(view, savedInstanceState)
+
+		/**
+		 * TODO adapter with items for downloading
+		adapter = CommonFriendsDialogAdapter()
+		binding.friendsList.adapter = adapter
+
+		adapter.submitList(commonFriends.map { it.contact })
+		 */
+
+		binding.confirmBtn.setDebouncedOnClickListener {
+			// todo download data with VM
+		}
+
+		binding.confirmBtn.setOnClickListener {
+			dismiss()
+		}
+	}
+}
