0a0e4f334cc69c503eabb33e34149755ffa2eb1f
Jan Novak <jan.novak@example.com>
2022-07-28 18:36:25 +0200

    groups_v3: implemented #2

--- profile/src/main/java/cz/cleevio/profile/groupFragment/GroupViewModel.kt
+++ profile/src/main/java/cz/cleevio/profile/groupFragment/GroupViewModel.kt
@@ -1,6 +1,7 @@
 package cz.cleevio.profile.groupFragment
 
 import androidx.lifecycle.viewModelScope
+import cz.cleevio.network.request.group.ImageRequest
 import cz.cleevio.network.data.Status
 import cz.cleevio.repository.model.group.Group
 import cz.cleevio.repository.repository.group.GroupRepository
@@ -8,16 +9,32 @@ import cz.cleevio.vexl.lightbase.core.baseClasses.BaseViewModel
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
+import java.util.concurrent.TimeUnit
+
+const val WEEK = 7L
 
 class GroupViewModel constructor(
 	val groupRepository: GroupRepository
 ) : BaseViewModel() {
 
+	//fixme: when we have UI for group creation
+	private val hardcodedLogo = ImageRequest(
+		extension = "png",
+		data = "YXNkYXNkc2FkYXNkYXM="
+	)
+	private val expiration = System.currentTimeMillis() + TimeUnit.DAYS.toMillis(WEEK)
+	private val closure = System.currentTimeMillis() + TimeUnit.DAYS.toMillis(WEEK)
+
 	val myGroups = groupRepository.getGroupsFlow()
 
 	fun createGroup(name: String, onSuccess: () -> Unit) {
 		viewModelScope.launch(Dispatchers.IO) {
-			groupRepository.createGroup(name = name)
+			groupRepository.createGroup(
+				name = name,
+				logo = hardcodedLogo,
+				expiration = expiration,
+				closureAt = closure
+			)
 
 			//reload
 			syncMyGroupsData()
