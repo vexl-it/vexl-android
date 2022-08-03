eae6c80c6a3da2776133862e1970f688b7a465c6
Tomáš Dvořáček <dvoracto@gmail.com>
2022-08-03 13:48:44 +0200

    username screen done

--- onboarding/src/main/java/cz/cleevio/onboarding/ui/usernameFragment/UsernameFragment.kt
+++ onboarding/src/main/java/cz/cleevio/onboarding/ui/usernameFragment/UsernameFragment.kt
@@ -1,16 +1,23 @@
 package cz.cleevio.onboarding.ui.usernameFragment
 
+import android.view.ViewGroup
 import android.widget.Toast
+import androidx.core.view.isVisible
+import androidx.core.view.marginBottom
+import androidx.core.view.updateLayoutParams
 import androidx.core.view.updatePadding
-import androidx.lifecycle.lifecycleScope
+import androidx.core.widget.doAfterTextChanged
 import androidx.navigation.fragment.findNavController
+import cz.cleevio.core.utils.repeatScopeOnStart
+import cz.cleevio.core.utils.safeNavigateWithTransition
+import cz.cleevio.core.utils.showKeyboard
 import cz.cleevio.core.utils.viewBinding
-import cz.cleevio.onboarding.BuildConfig
 import cz.cleevio.onboarding.R
 import cz.cleevio.onboarding.databinding.FragmentUsernameBinding
 import cz.cleevio.vexl.lightbase.core.baseClasses.BaseFragment
+import cz.cleevio.vexl.lightbase.core.extensions.hideKeyboard
+import cz.cleevio.vexl.lightbase.core.extensions.listenForIMEInset
 import cz.cleevio.vexl.lightbase.core.extensions.listenForInsets
-import kotlinx.coroutines.launch
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class UsernameFragment : BaseFragment(R.layout.fragment_username) {
@@ -19,10 +26,10 @@ class UsernameFragment : BaseFragment(R.layout.fragment_username) {
 	private val binding by viewBinding(FragmentUsernameBinding::bind)
 
 	override fun bindObservers() {
-		viewLifecycleOwner.lifecycleScope.launch {
+		repeatScopeOnStart {
 			viewModel.usernameAvailable.collect { usernameAvailable ->
 				if (usernameAvailable.available) {
-					findNavController().navigate(
+					findNavController().safeNavigateWithTransition(
 						UsernameFragmentDirections.proceedToAvatarFragment(usernameAvailable.username)
 					)
 				} else {
@@ -30,28 +37,38 @@ class UsernameFragment : BaseFragment(R.layout.fragment_username) {
 				}
 			}
 		}
+
+		repeatScopeOnStart {
+			viewModel.loading.collect {
+				binding.continueBtn.isEnabled = !it
+				binding.progressbar.isVisible = it
+			}
+		}
 	}
 
 	override fun initView() {
-		listenForInsets(binding.container) { insets ->
-			binding.container.updatePadding(
-				top = insets.top,
-				bottom = insets.bottomWithIME
-			)
-		}
+		binding.usernameInput.requestFocus()
+		binding.usernameInput.showKeyboard()
 
 		binding.continueBtn.setOnClickListener {
-			val username = binding.nameNameInput.text.toString()
-			if (username.isNotBlank()) {
-				viewModel.checkUsernameAvailability(username)
-			} else {
-				Toast.makeText(requireContext(), R.string.error_nickname_blank, Toast.LENGTH_SHORT).show()
-			}
+			binding.usernameInput.hideKeyboard()
+			val username = binding.usernameInput.text.toString()
+			viewModel.checkUsernameAvailability(username)
 		}
 
-		//debug
-		if (BuildConfig.DEBUG) {
-			binding.nameNameInput.setText("jakub_test_account")
+		binding.usernameInput.doAfterTextChanged {
+			binding.continueBtn.isEnabled = it.toString().isNotEmpty()
+		}
+
+		listenForInsets(binding.parent) { insets ->
+			binding.container.updatePadding(top = insets.top)
+		}
+
+		val defaultButtonMargin = binding.continueBtn.marginBottom
+		listenForIMEInset(binding.container) { bottomInset ->
+			binding.continueBtn.updateLayoutParams<ViewGroup.MarginLayoutParams> {
+				bottomMargin = bottomInset + defaultButtonMargin
+			}
 		}
 	}
 }
--- onboarding/src/main/java/cz/cleevio/onboarding/ui/usernameFragment/UsernameViewModel.kt
+++ onboarding/src/main/java/cz/cleevio/onboarding/ui/usernameFragment/UsernameViewModel.kt
@@ -5,22 +5,27 @@ import cz.cleevio.repository.model.user.UsernameAvailable
 import cz.cleevio.repository.repository.user.UserRepository
 import cz.cleevio.vexl.lightbase.core.baseClasses.BaseViewModel
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.flow.MutableSharedFlow
-import kotlinx.coroutines.flow.asSharedFlow
+import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.flow.receiveAsFlow
 import kotlinx.coroutines.launch
 
 class UsernameViewModel constructor(
 	private val userRepository: UserRepository
 ) : BaseViewModel() {
 
-	private val _usernameAvailable = MutableSharedFlow<UsernameAvailable>(replay = 1)
-	val usernameAvailable = _usernameAvailable.asSharedFlow()
+	private val _loading = Channel<Boolean>()
+	val loading = _loading.receiveAsFlow()
+
+	private val _usernameAvailable = Channel<UsernameAvailable>()
+	val usernameAvailable = _usernameAvailable.receiveAsFlow()
 
 	fun checkUsernameAvailability(username: String) {
 		viewModelScope.launch(Dispatchers.IO) {
+			_loading.send(true)
 			val response = userRepository.isUsernameAvailable(username)
+			_loading.send(false)
 			response.data?.let {
-				_usernameAvailable.emit(it)
+				_usernameAvailable.send(it)
 			}
 		}
 	}
