68a9686c856d1b8e1c1acf648bece999937ceab0
Tomáš Dvořáček <dvoracto@gmail.com>
2022-08-03 09:15:33 +0200

    faq done

--- onboarding/src/main/java/cz/cleevio/onboarding/ui/faqParentFragment/FAQParentFragment.kt
+++ onboarding/src/main/java/cz/cleevio/onboarding/ui/faqParentFragment/FAQParentFragment.kt
@@ -1,6 +1,7 @@
 package cz.cleevio.onboarding.ui.faqParentFragment
 
 import androidx.core.view.updatePadding
+import androidx.navigation.fragment.findNavController
 import androidx.viewpager2.widget.ViewPager2
 import cz.cleevio.core.utils.viewBinding
 import cz.cleevio.onboarding.R
@@ -16,8 +17,27 @@ class FAQParentFragment : BaseFragment(R.layout.fragment_faq_parent) {
 
 	override fun bindObservers() = Unit
 
+	private var adapter: PagerAdapter? = null
+
 	override fun initView() {
 		setupAdapter()
+
+		binding.close.setOnClickListener {
+			findNavController().popBackStack()
+		}
+
+		binding.faqParentLeftBtn.setOnClickListener {
+			when (binding.faqViewpager.currentItem) {
+				0 -> findNavController().popBackStack()
+				else -> binding.faqViewpager.setCurrentItem(binding.faqViewpager.currentItem - 1, true)
+			}
+		}
+		binding.faqParentRightBtn.setOnClickListener {
+			when (binding.faqViewpager.currentItem) {
+				(adapter?.itemCount ?: 0) - 1 -> findNavController().popBackStack()
+				else -> binding.faqViewpager.setCurrentItem(binding.faqViewpager.currentItem + 1, true)
+			}
+		}
 
 		listenForInsets(binding.container) { insets ->
 			binding.container.updatePadding(top = insets.top, bottom = insets.bottom)
@@ -25,7 +45,7 @@ class FAQParentFragment : BaseFragment(R.layout.fragment_faq_parent) {
 	}
 
 	private fun setupAdapter() {
-		val adapter = PagerAdapter(requireActivity())
+		adapter = PagerAdapter(requireActivity())
 		binding.faqViewpager.adapter = adapter
 		binding.faqViewpager.registerOnPageChangeCallback(object : ViewPager2.OnPageChangeCallback() {
 			override fun onPageSelected(position: Int) {
@@ -36,7 +56,7 @@ class FAQParentFragment : BaseFragment(R.layout.fragment_faq_parent) {
 						binding.faqParentLeftBtn.text = getString(R.string.general_close)
 						binding.faqParentRightBtn.text = getString(R.string.next)
 					}
-					adapter.itemCount - 1 -> {
+					(adapter?.itemCount ?: 0) - 1 -> {
 						binding.faqParentLeftBtn.text = getString(R.string.back)
 						binding.faqParentRightBtn.text = getString(R.string.general_done)
 					}
