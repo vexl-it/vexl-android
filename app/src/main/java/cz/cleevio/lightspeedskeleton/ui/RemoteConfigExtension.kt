96ade5e478d1701b7d1ae4561e68a465af5bb879
karolinaklepackova <klepackova.karolina@gmail.com>
2022-10-24 09:57:41 +0200

    IFs removed

--- app/src/main/java/cz/cleevio/lightspeedskeleton/ui/RemoteConfigExtension.kt
+++ app/src/main/java/cz/cleevio/lightspeedskeleton/ui/RemoteConfigExtension.kt
@@ -7,18 +7,14 @@ import kotlin.coroutines.suspendCoroutine
 
 suspend fun checkForUpdate(remoteConfig: FirebaseRemoteConfig): Boolean =
 	suspendCoroutine { continuation ->
-		remoteConfig.fetchAndActivate().addOnCompleteListener { activatedTask ->
-			if (activatedTask.isSuccessful) {
-				continuation.resume(remoteConfig.getBoolean(RemoteConfigConstants.FORCE_UPDATE_SHOWED))
-			}
+		remoteConfig.fetchAndActivate().addOnCompleteListener {
+			continuation.resume(remoteConfig.getBoolean(RemoteConfigConstants.FORCE_UPDATE_SHOWED))
 		}
 	}
 
 suspend fun checkForMaintenance(remoteConfig: FirebaseRemoteConfig): Boolean =
 	suspendCoroutine { continuation ->
-		remoteConfig.fetchAndActivate().addOnCompleteListener { activatedTask ->
-			if (activatedTask.isSuccessful) {
-				continuation.resume(remoteConfig.getBoolean(RemoteConfigConstants.MAINTENANCE_SCREEN_SHOWED))
-			}
+		remoteConfig.fetchAndActivate().addOnCompleteListener {
+			continuation.resume(remoteConfig.getBoolean(RemoteConfigConstants.MAINTENANCE_SCREEN_SHOWED))
 		}
 	}
