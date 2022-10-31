cd7dbb68560ccc1d1201f76ece491cfe57138611
karolinaklepackova <klepackova.karolina@gmail.com>
2022-10-31 15:26:11 +0100

    New clib added

--- cryptography/cryptography/include/vc/common/Constants.h
+++ cryptography/cryptography/include/vc/common/Constants.h
@@ -8,7 +8,7 @@
 #define SALT (const unsigned char*)"vexlvexl"
 #define SALT_LEN (int)8
 #define PBKDF2ITER 2000
-#define MAX_DATA_SIZE_LIMIT (int)150000000
+#define MAX_DATA_SIZE_LIMIT (int)15000000
 #define MAX_CIPHER_SIZE_LIMIT ((double)MAX_DATA_SIZE_LIMIT)*(4.0/3.0)
 
 #endif
