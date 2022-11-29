ab81cace5c5d933664ef4272edeceddb04c432ac
Jan Novak <jan.novak@example.com>
2022-11-29 10:44:03 +0100

    VEX-1423: implemented #1

--- core/src/main/java/cz/cleevio/core/base/BaseFacebookContactsListViewModel.kt
+++ core/src/main/java/cz/cleevio/core/base/BaseFacebookContactsListViewModel.kt
@@ -1,7 +1,6 @@
 package cz.cleevio.core.base
 
 import androidx.lifecycle.viewModelScope
-import com.facebook.AccessToken
 import cz.cleevio.core.utils.NavMainGraphModel
 import cz.cleevio.network.data.Status
 import cz.cleevio.repository.model.contact.BaseContact
@@ -27,12 +26,12 @@ open class BaseFacebookContactsListViewModel constructor(
 	val uploadSuccessful = _uploadSuccessful.asSharedFlow()
 
 	fun loadNotSyncedFacebookContacts() {
-		AccessToken.getCurrentAccessToken()?.let {
-			loadNotSyncedFacebookContacts(
-				it.userId,
-				it.token
-			)
-		}
+//		AccessToken.getCurrentAccessToken()?.let {
+//			loadNotSyncedFacebookContacts(
+//				it.userId,
+//				it.token
+//			)
+//		}
 	}
 
 	private fun loadNotSyncedFacebookContacts(facebookId: String, accessToken: String) {
